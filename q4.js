4af7b551cdef99dd57949d00f5d140c9a6e7b16d
FrekeNobles <noblendifreke@gmail.com>
2026-04-20 01:04:13 +0100

    add q5

--- q4.js
+++ q4.js
@@ -1,8 +1,8 @@
 // Array
-const fruits = ["mango", "orange", "banana"]
-    fruits.push("pineapple")
-    fruits.shift()
-    
- console.log(fruits)
- console.log(fruits.length)
+const fruits = ["mango", "orange", "banana"];
+    fruits.push("pineapple");
+    fruits.shift();
+
+ console.log(fruits);
+ console.log(fruits.length);
  
---
+++ q5.js
@@ -0,0 +1,9 @@
+//map method
+
+const numbers = [3, 6, 9, 12, 15];
+const doubleNumbers = numbers.map((num)=> {
+    return num * 2;
+})
+
+console.log(numbers)
+console.log(doubleNumbers)
